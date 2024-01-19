09a2c39ba0e0cbcc89a5c10fa56a6a29d2ea5cf9
pravallika.buddha <137508600+pravaligithub@users.noreply.github.com>
2024-01-19 19:59:26 +0530

    Add files via upload

---
+++ frontened/javascript var,operators,datatypes.js
@@ -0,0 +1,22 @@
+// Variable Declarations
+let x, y, sum;
+ 
+// Assign value to the variables
+x = 3;
+y = 23;
+ 
+// Use arithmetic operator to
+// add two numbers
+sum = x + y;
+ 
+console.log(sum);
+
+
+const a = "Hello learners"
+console.log(a);
+
+var b = 10;
+var c = 12;
+var d = b + c;
+ 
+console.log(d);
